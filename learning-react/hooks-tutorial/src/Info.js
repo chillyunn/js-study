18b4cdaf05c06ba44522d09b3a086b2ce0e29236
chillyunn <dkwhkp0@naver.com>
2022-07-12 16:45:37 +0900

    ch08.Hooks

---
+++ learning-react/hooks-tutorial/src/Info.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+//   useEffect(() => {
+//     console.log("렌더링때 마다 실행됩니다.");
+//     console.log({ name, nickname });
+//   });
+    useEffect(() => {
+      console.log('마운트될 때만 실행됩니다.');
+  },[])
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeNickName = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickName} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
